ed78ec6f1129365cb6d39f2f623af7274c698903
Dustin Larimer <dustinlarimer@gmail.com>
2014-03-23 14:07:26 -0700

    update shell script

--- config/sauce_connect_setup.sh
+++ config/sauce_connect_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Setup and start Sauce Connect for your TravisCI build
 # This script requires your .travis.yml to include the following two private env variables:
 # SAUCE_USERNAME
