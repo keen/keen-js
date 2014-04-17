1c2dd58ee21c3d63e6e20bd6712e2dae41eeb522
Dustin Larimer <dustinlarimer@gmail.com>
2014-04-16 17:57:51 -0700

    Update CS with dataform that handles flat arrays (funnels)

--- src/plugins/keen-chartstack.js
+++ src/plugins/keen-chartstack.js
@@ -7,12 +7,18 @@
 
   !function(name, context){
     var Keen = context[name] || {};
-    /*var cs = context.chartstack;
-    Keen.chartstack = cs.noConflict();
+    var cs = context.chartstack;
+    Keen.vis = cs; //.noConflict();
     Keen.Chart = function(obj){
-      return new Keen.chartstack.Chart(obj);
+      return new Keen.vis.Chart(obj);
     };
-    // Keen.ready = cs.ready;*/
+    Keen.ready = Keen.vis.ready;
+    Keen.palette = [
+      '#00afd7', // blue
+      '#49c5b1', // green
+      '#e6b449', // gold
+      '#f35757'  // red
+    ];
 
     // -------------------------------
     // Inject <query>.draw Method
@@ -35,28 +41,13 @@
 
     Keen.Visualization = function(req, selector, config){
       var options = (config || {}), recommended;
-      var library = chartstack.libraries[options.library] || chartstack.library;
+      var library = Keen.vis.libraries[options.library] || Keen.vis.library;
 
       var datasetConfig = {
         adapter: "keen-io",
         url: req.instance.client.endpoint + '/projects/' + req.instance.client.projectId + req.queries[0].path,
         dateformat: config.dateFormat || "",
         params: req.queries[0].params
-        /*
-        format: {
-          index: { type: 'date', sort: 'desc', format: 'MMM DDD' }
-        },
-        map: {
-          root: "result",
-          each: {
-            index
-          },
-          sort: {
-            index: 'asc',
-            label: 'desc'
-          }
-        }
-        */
       };
       datasetConfig.params.api_key = req.instance.client.readKey;
 
@@ -64,41 +55,84 @@
         el: selector,
         chartOptions: config.chartOptions || {}
       };
-
-
+      viewConfig.chartOptions.colors = viewConfig.chartOptions.colors || Keen.palette;
 
       if (req.queries[0].params.interval) { // Series
         options.capable = ['areachart', 'barchart', 'columnchart', 'linechart', 'table'];
         recommended = 'linechart';
       } else {
         if (req.queries[0].params.group_by) { // Static
-          options.capable = ['piechart', 'table'];
-          recommended = 'piechart';
+          options.capable = ['piechart', 'barchart', 'columnchart', 'table'];
+          recommended = 'barchart';
         } else { // Metric
           options.capable = ['metric'];
           recommended = 'metric';
         }
       }
+
+      // 2x Group_by handler
+      if (req.queries[0].params.group_by instanceof Array) {
+        options.capable = ['areachart', 'barchart', 'columnchart', 'linechart', 'table'];
+        recommended = 'columnchart';
+        datasetConfig.map = {
+          root: 'result',
+          //each: {},
+          sort: {
+            index: 'asc',
+            label: 'desc'
+          }
+        };
+        if (req.queries[0].params.interval) {
+          datasetConfig.map.each = {
+            label: 'value -> ' + req.queries[0].params.group_by[0],
+            index: 'timeframe -> start',
+            value: 'value -> result'
+          };
+        } else {
+          datasetConfig.map.each = {
+            label: req.queries[0].params.group_by[1],
+            index: req.queries[0].params.group_by[0],
+            value: 'result'
+          };
+        }
+      }
+
+      if (req.queries[0].params.steps) {
+        options.capable = ['areachart', 'barchart', 'columnchart', 'linechart', 'table'];
+        recommended = 'columnchart';
+        /*datasetConfig.map = {
+          root: '',
+          each: {
+            //index: 'steps -> event_collection',
+            label: 'steps -> event_collection',
+            value: 'result'
+          },
+          sort: {
+            index: 'asc'
+            //label: 'desc'
+          }
+        };*/
+      }
+
+
       options.chartType = options.chartType || recommended;
-      // if (options.chartType && this.capable.indexOf(options.chartType)) -> request is going to work
 
       if (options.chartType == 'metric') {
-        library = 'widgets';
-        // return new Keen.Number(selector, config);
-        // return new chartstack.Widgets.Number({ });
+        library = 'keen-io';
       }
 
       if (library) {
-        if (chartstack.libraries[library][options.chartType]) {
-          return new chartstack.Chart({
-            dataset: new chartstack.Dataset(datasetConfig),
-            view: new chartstack.libraries[library][options.chartType](viewConfig)
+        if (Keen.vis.libraries[library][options.chartType]) {
+          return new Keen.Chart({
+            dataset: new Keen.vis.Dataset(datasetConfig),
+            view: new Keen.vis.libraries[library][options.chartType](viewConfig)
           })
 
         } else {
           Keen.log('The visualization type you requested is not available for this library');
         }
       } else {
+        console.log(req.queries[0].params.group_by instanceof Array);
         Keen.log('The visualization library you requested is not present');
       }
 
