7715114b59cd14f04a1e5407afb727f53e1c9213
Dustin Larimer <dustinlarimer@gmail.com>
2014-04-18 11:12:26 -0700

    Query get/set, requestType defaults, camelCase conversion

--- src/intro.js
+++ src/intro.js
@@ -5,53 +5,53 @@
 }('Keen', this, function() {
   'use strict';
 
-  /*! 
+  /*!
   * ----------------
   * Keen IO Core JS
   * ----------------
   */
-  
+
   function Keen(config) {
     return _init.apply(this, arguments);
   }
-  
+
   function _init(config) {
     if (_isUndefined(config)) {
       throw new Error("Check out our JavaScript SDK Usage Guide: https://keen.io/docs/clients/javascript/usage-guide/");
     }
-    if (_isUndefined(config.projectId) || typeof config.projectId !== 'string' || config.projectId.length < 1) {
+    if (_isUndefined(config.projectId) || _type(config.projectId) !== 'String' || config.projectId.length < 1) {
       throw new Error("Please provide a projectId");
     }
-    
+
     this.configure(config);
   }
-  
+
   Keen.prototype.configure = function(config){
-    
+
     config['host'] = (_isUndefined(config['host'])) ? 'api.keen.io/3.0' : config['host'].replace(/.*?:\/\//g, '');
     config['protocol'] = _set_protocol(config['protocol']);
     config['requestType'] = _set_request_type(config['requestType']);
-    
+
     this.client = {
       projectId: config.projectId,
       writeKey: config.writeKey,
       readKey: config.readKey,
       globalProperties: null,
-      
+
       endpoint: config['protocol'] + "://" + config['host'],
       requestType: config['requestType']
     };
-    
+
     Keen.trigger('client', this, config);
     this.trigger('ready');
-    
+
     return this;
   };
-  
-  
+
+
   // Private
   // --------------------------------
-  
+
   function _extend(target){
     for (var i = 1; i < arguments.length; i++) {
       for (var prop in arguments[i]){
@@ -61,16 +61,42 @@
     }
     return target;
   }
-  
+
   function _isUndefined(obj) {
     return obj === void 0;
   }
-  
+
   function _type(obj){
 	  var text = obj.constructor.toString();
 	  return text.match(/function (.*)\(/)[1];
   }
-  
+
+  function _each(o, cb, s){
+    var n;
+    if (!o){
+      return 0;
+    }
+    s = !s ? o : s;
+    if (_type(o)==='array'){ // is(o.length)
+      // Indexed arrays, needed for Safari
+      for (n=0; n<o.length; n++) {
+        if (cb.call(s, o[n], n, o) === false){
+          return 0;
+        }
+      }
+    } else {
+      // Hashtables
+      for (n in o){
+        if (o.hasOwnProperty(n)) {
+          if (cb.call(s, o[n], n, o) === false){
+            return 0;
+          }
+        }
+      }
+    }
+    return 1;
+  }
+
   function _set_protocol(value) {
     switch(value) {
       case 'http':
@@ -86,15 +112,16 @@
         break;
     }
   }
-  
+
   function _set_request_type(value) {
-    var configured = value || 'xhr';
+    var configured = value || 'jsonp';
     var capableXHR = false;
-    if ((typeof XMLHttpRequest === 'object' || typeof XMLHttpRequest === 'function') && 'withCredentials' in new XMLHttpRequest()) {
+    //if ((typeof XMLHttpRequest === 'object' || typeof XMLHttpRequest === 'function') && 'withCredentials' in new XMLHttpRequest()) {
+    if ((_type(XMLHttpRequest)==='Object'||_type(XMLHttpRequest)==='Function') && 'withCredentials' in new XMLHttpRequest()) {
       capableXHR = true;
     }
     //var capableXHR = (void 0 !== XMLHttpRequest && 'withCredentials' in new XMLHttpRequest());
-    
+
     if (configured == null || configured == 'xhr') {
       if (capableXHR) {
         return 'xhr';
@@ -105,31 +132,28 @@
       return configured;
     }
   }
-  
+
   function _build_url(path) {
     return this.client.endpoint + '/projects/' + this.client.projectId + path;
   }
-  
-  
+
+
   var _request = {
-    
-    xhr: function(method, url, headers, body, apiKey, success, error, sequence){
+
+    xhr: function(method, url, headers, body, apiKey, success, error){
       if (!apiKey) return Keen.log('Please provide a writeKey for https://keen.io/project/' + this.client.projectId);
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
           if (xhr.status >= 200 && xhr.status < 300) {
-            var response, meta = {};
+            var response;
             try {
               response = JSON.parse(xhr.responseText);
-              if (typeof sequence == 'number') {
-                meta.sequence = sequence;
-              }
             } catch (e) {
               Keen.log("Could not JSON parse HTTP response: " + xhr.responseText);
               if (error) error(xhr, e);
             }
-            if (success && response) success(response, meta);
+            if (success && response) success(response);
           } else {
             Keen.log("HTTP request failed.");
             if (error) error(xhr, null);
@@ -147,28 +171,23 @@
       var toSend = body ? JSON.stringify(body) : null;
       xhr.send(toSend);
     },
-    
-    jsonp: function(url, apiKey, success, error, sequence){
+
+    jsonp: function(url, apiKey, success, error){
       if (!apiKey) return Keen.log('Please provide a writeKey for https://keen.io/project/' + this.client.projectId);
-      // Fall back to JSONP for GET and sending data base64 encoded for POST
-      // Add api_key if it's not there
       if (apiKey && url.indexOf("api_key") < 0) {
         var delimiterChar = url.indexOf("?") > 0 ? "&" : "?";
         url = url + delimiterChar + "api_key=" + apiKey;
       }
-      // JSONP
+
       var callbackName = "keenJSONPCallback" + new Date().getTime();
       while (callbackName in window) {
         callbackName += "a";
       }
-      var loaded = false, meta;
+      var loaded = false;
       window[callbackName] = function (response) {
-        loaded = true, meta = {};
+        loaded = true;
         if (success && response) {
-          if (typeof sequence == 'number') {
-            meta.sequence = sequence;
-          }
-          success(response, meta);
+          success(response);
         };
         // Remove this from the namespace
         window[callbackName] = undefined;
@@ -193,7 +212,7 @@
         }
       }
     },
-    
+
     beacon: function(url, apiKey, success, error){
       if (apiKey && url.indexOf("api_key") < 0) {
         var delimiterChar = url.indexOf("?") > 0 ? "&" : "?";
@@ -218,12 +237,12 @@
       img.src = url;
     }
   };
-  
-  
+
+
   // -------------------------------
   // Keen.Events
   // -------------------------------
-  
+
   var Events = Keen.Events = {
     on: function(name, callback) {
       this.listeners || (this.listeners = {});
@@ -259,15 +278,14 @@
   };
   _extend(Keen.prototype, Events);
   _extend(Keen, Events);
-  
-  
+
+
   Keen.ready = function(callback){
     Keen.on('ready', callback);
   }
-  
+
   // -------------------------------
   // Keen.Plugins
   // -------------------------------
-  
+
   var Plugins = Keen.Plugins = {};
-  
