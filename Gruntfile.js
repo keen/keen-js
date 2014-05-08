6527ef7d7797864500e2a79f1d3d06d3efcd692b
Dustin Larimer <dustinlarimer@gmail.com>
2014-05-07 17:33:16 -0700

    S3 deployment with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
-var saucelabs = require('./config/saucelabs')();
+var saucelabs = require('./config/saucelabs')(),
+    aws = require('./config/aws')();
 
 module.exports = function(grunt) {
 
@@ -6,6 +7,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-concat");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-s3');
 
   grunt.loadNpmTasks('grunt-saucelabs');
 
@@ -34,7 +36,7 @@ module.exports = function(grunt) {
           "src/lib/chartstack.js",
           "src/plugins/keen-chartstack.js"
         ],
-        dest: "dist/<%= pkg.name %>-<%= pkg.version %>.js"
+        dest: "dist/<%= pkg.name %>.js"
       },
       track: {
         src: [
@@ -46,7 +48,7 @@ module.exports = function(grunt) {
           "src/outro.js"
           //"src/plugins/keen-pageviews.js"
         ],
-        dest: "dist/<%= pkg.name %>-<%= pkg.version %>.track.js"
+        dest: "dist/<%= pkg.name %>-track.js"
       },
       query: {
         src: [
@@ -57,7 +59,7 @@ module.exports = function(grunt) {
           "src/async.js",
           "src/outro.js"
         ],
-        dest: "dist/<%= pkg.name %>-<%= pkg.version %>.query.js"
+        dest: "dist/<%= pkg.name %>-query.js"
       },
       visualize: {
         src: [
@@ -72,11 +74,11 @@ module.exports = function(grunt) {
           "src/lib/chartstack.js",
           "src/plugins/keen-chartstack.js"
         ],
-        dest: "dist/<%= pkg.name %>-<%= pkg.version %>.visualize.js"
+        dest: "dist/<%= pkg.name %>-visualize.js"
       },
       loader: {
         src: "src/loader.js",
-        dest: "dist/<%= pkg.name %>-<%= pkg.version %>.loader.js"
+        dest: "dist/<%= pkg.name %>-loader.js"
       }
     },
 
@@ -88,11 +90,11 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          "dist/<%= pkg.name %>-<%= pkg.version %>.min.js": "dist/<%= pkg.name %>-<%= pkg.version %>.js",
-          "dist/<%= pkg.name %>-<%= pkg.version %>.track.min.js": "dist/<%= pkg.name %>-<%= pkg.version %>.track.js",
-          "dist/<%= pkg.name %>-<%= pkg.version %>.query.min.js": "dist/<%= pkg.name %>-<%= pkg.version %>.query.js",
-          "dist/<%= pkg.name %>-<%= pkg.version %>.visualize.min.js": "dist/<%= pkg.name %>-<%= pkg.version %>.visualize.js",
-          "dist/<%= pkg.name %>-<%= pkg.version %>.loader.min.js": "dist/<%= pkg.name %>-<%= pkg.version %>.loader.js"
+          "dist/<%= pkg.name %>.min.js": "dist/<%= pkg.name %>.js",
+          "dist/<%= pkg.name %>-track.min.js": "dist/<%= pkg.name %>-track.js",
+          "dist/<%= pkg.name %>-query.min.js": "dist/<%= pkg.name %>-query.js",
+          "dist/<%= pkg.name %>-visualize.min.js": "dist/<%= pkg.name %>-visualize.js",
+          "dist/<%= pkg.name %>-loader.min.js": "dist/<%= pkg.name %>-loader.js"
         }
       }
     },
@@ -124,6 +126,29 @@ module.exports = function(grunt) {
           browsers: saucelabs.browsers,
           concurrency: saucelabs.concurrency
         }
+      }
+    },
+
+    s3: {
+      options: {
+        key: aws.key,
+        secret: aws.secret,
+        bucket: aws.bucket,
+        access: 'public-read',
+        headers: {
+          // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+          "Cache-Control": "max-age=630720000, public",
+          "Expires": new Date(Date.now() + 63072000000).toUTCString()
+        },
+        gzip: true
+      },
+      deploy: {
+        sync: [
+          {
+            src: 'dist/*',
+            dest: '<%= pkg.version %>/'
+          }
+        ]
       }
     }
 
