50d6fef41d3867acea9cffa1fa123e60d7805bb6
Dustin Larimer <dustinlarimer@gmail.com>
2014-05-19 21:54:44 -0700

    Default to area chart instead of line chart, resolves #49

--- src/plugins/keen-chartstack.js
+++ src/plugins/keen-chartstack.js
@@ -59,7 +59,7 @@
             type: "number",
             label: "Metric",
             format: false,
-            method: "Keen.prettyNumber",
+            method: "Keen.utils.prettyNumber",
             replace: {
               null: 0
             }
@@ -276,7 +276,7 @@
       // Single Interval
       if (isInterval) { // Series
         options.capable = ['areachart', 'barchart', 'columnchart', 'linechart', 'datatable'];
-        recommended = 'linechart';
+        recommended = 'areachart';
         if (!isGroupBy && library == 'google') {
           viewConfig.chartOptions.legend = { position: 'none' };
         }
@@ -392,41 +392,31 @@
       return this;
     };
 
-    Keen.prettyNumber = function(input) {
-
+    Keen.utils.prettyNumber = function(input) {
       // If it has 3 or fewer sig figs already, just return the number.
       var sciNo = input.toPrecision(3),
           prefix = "",
           suffixes = ["", "k", "M", "B", "T"];
-
       if (Number(sciNo) == input && String(input).length <= 4) {
         return String(input);
       }
-
       if(input >= 1 || input <= -1) {
-
         if(input < 0){
             //Pull off the negative side and stash that.
             input = -input;
             prefix = "-";
         }
-
         function recurse(input, iteration) {
-
           var input = String(input);
           split = input.split(".");
-
           // If there's a dot
           if(split.length > 1) {
-
             // Keep the left hand side only
             input = split[0];
             var rhs = split[1];
-
             // If the left-hand side is too short, pad until it has 3 digits
             if (input.length == 2 && rhs.length > 0) {
-
-                // Pad with right-hand side if possible
+              // Pad with right-hand side if possible
               if (rhs.length > 0) {
                 input = input + "." + rhs.charAt(0);
               }
@@ -437,7 +427,6 @@
             }
             else if (input.length == 1 && rhs.length > 0) {
               input = input + "." + rhs.charAt(0);
-
               // Pad with right-hand side if possible
               if(rhs.length > 1) {
                 input += rhs.charAt(1);
@@ -448,14 +437,11 @@
               }
             }
           }
-
           var numNumerals = input.length;
-
           // if it has a period, then numNumerals is 1 smaller than the string length:
           if (input.split(".").length > 1) {
             numNumerals--;
           }
-
           if(numNumerals <= 3) {
             return String(input) + suffixes[iteration];
           }
@@ -463,9 +449,7 @@
             return recurse(Number(input) / 1000, iteration + 1);
           }
         }
-
         return prefix + recurse(input, 0);
-
       } else {
         return input.toPrecision(3);
       }
