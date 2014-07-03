8c1c7c8bd5154db8e73ca5592f4da774bd5c46ca
Dustin Larimer <dustinlarimer@gmail.com>
2014-07-02 17:10:20 -0700

    Debug type check function, fixes #96

--- src/core.js
+++ src/core.js
@@ -60,8 +60,16 @@
   }
 
   function _type(obj){
-    var text = (obj && obj.constructor) ? obj.constructor.toString() : void 0;
-	  return (text) ? text.match(/function (.*)\(/)[1] : "Null";
+    var text, parsed;
+    text = (obj && obj.constructor) ? obj.constructor.toString() : void 0;
+    if (text) {
+      parsed = text.split("(")[0].split(/function\s*/);
+      if (parsed.length > 0) {
+        return parsed[1];
+      }
+    }
+    return "Null";
+	  //return (text) ? text.match(/function (.*)\(/)[1] : "Null";
   }
 
   function _each(o, cb, s){
