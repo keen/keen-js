8449d8946c40b0e861c074ebfd4ae52aee017c7b
Sean Dokko <dokko1230@gmail.com>
2014-08-20 14:52:05 -0700

    Remove redundancy in adaptor

--- src/plugins/keen-googlecharts.js
+++ src/plugins/keen-googlecharts.js
@@ -26,7 +26,7 @@
             packages: ['corechart', 'table'],
             callback: function(){
               Keen.loaded = true;
-              Keen.trigger('ready');
+              // Keen.trigger('ready');
             }
         });
       }
@@ -37,195 +37,42 @@
       this.trigger('error', message);
     }
 
-    function handleRemoval(){
-      var self = this;
-      google.visualization.events.removeAllListeners(self._chart);
-      self._chart.clearChart();
-    }
+    var chartTypes = ['AreaChart', 'BarChart', 'ColumnChart', 'LineChart', 'PieChart', 'Table'];
+    var charts = {};
 
 
     // Create chart types
     // -------------------------------
-
-    AreaChart = Keen.Visualization.extend({
-      initialize: function(){
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        self._chart = self._chart || new google.visualization.AreaChart(self.el);
-        google.visualization.events.addListener(self._chart, 'error', function(stack){
-          handleErrors.call(self, stack);
-        });
-        this.update();
-      },
-      update: function(){
-        this.remove();
-        var data = google.visualization.arrayToDataTable(this.data.table);
-        var options = Keen.utils.extend(this.chartOptions, {
-          title: this.title || '',
-          height: parseInt(this.height),
-          width: parseInt(this.width),
-          colors: this.colors
-        });
-        this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
-    });
-
-    BarChart = Keen.Visualization.extend({
-      initialize: function(){
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        self._chart = self._chart || new google.visualization.BarChart(self.el);
-        google.visualization.events.addListener(self._chart, 'error', function(stack){
-          handleErrors.call(self, stack);
-        });
-        self.update();
-      },
-      update: function(){
-        this.remove();
-        var data = google.visualization.arrayToDataTable(this.data.table);
-        var options = Keen.utils.extend(this.chartOptions, {
-          title: this.title || '',
-          height: parseInt(this.height),
-          width: parseInt(this.width),
-          colors: this.colors
-        });
-        this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
-    });
-
-    ColumnChart = Keen.Visualization.extend({
-      initialize: function(){
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        self._chart = self._chart || new google.visualization.ColumnChart(self.el);
-        google.visualization.events.addListener(self._chart, 'error', function(stack){
-          handleErrors.call(self, stack);
-        });
-        self.update();
-      },
-      update: function(){
-        this.remove();
-        var data = google.visualization.arrayToDataTable(this.data.table);
-        var options = Keen.utils.extend(this.chartOptions, {
-          title: this.title || '',
-          height: parseInt(this.height),
-          width: parseInt(this.width),
-          colors: this.colors
-        });
-        this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
-    });
-
-    LineChart = Keen.Visualization.extend({
-      initialize: function(){
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        self._chart = self._chart || new google.visualization.LineChart(self.el);
-        google.visualization.events.addListener(self._chart, 'error', function(stack){
-          handleErrors.call(self, stack);
-        });
-        self.update();
-      },
-      update: function(){
-        this.remove();
-        var data = google.visualization.arrayToDataTable(this.data.table);
-        var options = Keen.utils.extend(this.chartOptions, {
-          title: this.title || '',
-          height: parseInt(this.height),
-          width: parseInt(this.width),
-          colors: this.colors
-        });
-        this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
-    });
-
-    PieChart = Keen.Visualization.extend({
-      initialize: function(){
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        self._chart = self._chart || new google.visualization.PieChart(self.el);
-        google.visualization.events.addListener(self._chart, 'error', function(stack){
-          handleErrors.call(self, stack);
-        });
-        self.update();
-      },
-      update: function(){
-        this.remove();
-        var data = google.visualization.arrayToDataTable(this.data.table);
-        var options = Keen.utils.extend(this.chartOptions, {
-          title: this.title || '',
-          height: parseInt(this.height),
-          width: parseInt(this.width),
-          colors: this.colors
-        });
-        this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
-    });
-
-    Table = Keen.Visualization.extend({
-      initialize: function(){
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        self._chart = self._chart || new google.visualization.Table(self.el);
-        google.visualization.events.addListener(self._chart, 'error', function(stack){
-          handleErrors.call(self, stack);
-        });
-        self.update();
-      },
-      update: function(){
-        this.remove();
-        var data = google.visualization.arrayToDataTable(this.data.table);
-        var options = Keen.utils.extend(this.chartOptions, {
-          title: this.title || '',
-          height: parseInt(this.height),
-          width: parseInt(this.width),
-          colors: this.colors
-        });
-        this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
+    _each(chartTypes, function (chart) {
+      charts[chart.toLowerCase()] = Keen.Visualization.extend({
+        initialize: function(){
+          this.render();
+        },
+        render: function(){
+          var self = this;
+          self._chart = self._chart || new google.visualization[chart](self.el);
+          google.visualization.events.addListener(self._chart, 'error', function(stack){
+            handleErrors.call(self, stack);
+          });
+          this.update();
+        },
+        update: function(){
+          var data = google.visualization.arrayToDataTable(this.data.table);
+          var options = Keen.utils.extend(this.chartOptions, {
+            title: this.title || '',
+            height: parseInt(this.height),
+            width: parseInt(this.width),
+            colors: this.colors
+          });
+          this._chart.draw(data, options);
+        }
+      });
     });
 
 
     // Register library + types
     // -------------------------------
 
-    Keen.Visualization.register('google', {
-      'areachart'   : AreaChart,
-      'barchart'    : BarChart,
-      'columnchart' : ColumnChart,
-      'linechart'   : LineChart,
-      'piechart'    : PieChart,
-      'table'       : Table
-    });
+    Keen.Visualization.register('google', charts);
 
   })(Keen);
