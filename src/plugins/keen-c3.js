51169b5ca2c28f3bcc0523cb876b03a5ac456de3
Sean Dokko <dokko1230@gmail.com>
2014-08-20 15:41:32 -0700

    Make a maker function to produce different types of charts

--- src/plugins/keen-c3.js
+++ src/plugins/keen-c3.js
@@ -15,12 +15,14 @@
         Table;
 
     var errors = {
-    }
+    };
 
     Keen.utils.loadScript("http://cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js", function() {
-      Keen.utils.loadScript("http://cdnjs.cloudflare.com/ajax/libs/c3/0.1.29/c3.min.js", function() {
+      Keen.utils.loadScript("http://c3js.org/js/c3.min-05d32fdf.js", function() {
         Keen.loaded = true;
         Keen.trigger('ready');
+        Keen.utils.loadStyle("http://c3js.org/css/c3-f750e4d4.css", function() {
+        });
       });
     });
 
@@ -35,65 +37,93 @@
       self._chart.clearChart();
     }
 
+    /**
+     * Unpacks the data from dataform's table. Basically, it takes the table and rotates it
+     * 90 degrees.
+     * @param  {[2D array]} table [the dataform 2d array]
+     * @return {[2D array]}       [the resulting array that is compatible with C3's column structure]
+     */
+    var _unpack = function(table) {
+      var plucked = [];
+      var numberOfColumns = table[0].length;
+      // Construct new table
+      for(var x = 0; x < numberOfColumns; x++) {
+        plucked.push([]);
+      }
+      // Build new table that is compatible with c3.
+      // The first item in the table will be the names
+      for(var i = 0; i < table.length; i++) {
+        for(var j = 0; j < numberOfColumns; j++) {
+          plucked[j].push(table[i][j]);
+        }
+      }
+      return plucked;
+    };
+
+    var chartTypes = ['Spline', 'Pie', 'Donut', 'Area-Spline', 'Bar', 'Scatter'];
+    var charts = {};
 
     // Create chart types
     // -------------------------------
 
-    LineChart = Keen.Visualization.extend({
-      initialize: function(){
-        debugger;
-        this.render();
-      },
-      render: function(){
-        var self = this;
-        debugger;
+    _each(chartTypes, function (chart) {
+      charts[chart] = Keen.Visualization.extend({
+        initialize: function(){
+          this.data.c3 = _unpack(this.data.table);
+          this.render();
+        },
+        render: function(){
+          var self = this;
 
-        // self._chart = self._chart || new google.visualization.AreaChart(self.el);
-        self._chart = c3.generate({
-          bindto: self.el,
-          grid: {
-            x: {
-              show: true
+          // self._chart = self._chart || new google.visualization.AreaChart(self.el);
+          self._chart = c3.generate({
+            bindto: self.el,
+            grid: {
+              x: {
+                show: true
+              },
+              y: {
+                show: true
+              }
             },
-            y: {
-              show: true
-            }
-          },
-          data: {
-            x: 'x',
-            columns: [
-              ['x'].concat(dates),
-              [this.model.get('name')].concat(weights)
-            ]
-          }
-        });
-        // google.visualization.events.addListener(self._chart, 'error', function(stack){
-        //   handleErrors.call(self, stack);
-        // });
-        this.update();
-      },
-      update: function(){
-        // this.remove();
-        // var data = google.visualization.arrayToDataTable(this.data.table);
-        // var options = Keen.utils.extend(this.chartOptions, {
-        //   title: this.title || '',
-        //   height: parseInt(this.height),
-        //   width: parseInt(this.width),
-        //   colors: this.colors
-        // });
-        // this._chart.draw(data, options);
-      },
-      remove: function(){
-        handleRemoval.call(this);
-      }
-    });
+            data: {
+              x: this.data.c3[0][0],
+              columns: this.data.c3,
+              type: chart.toLowerCase()
+            },
+            axis: (function() {
+              var candidates = self.data.c3;
+              // If the first itemset is a date, then make it into a timeseries
+              if(candidates[0][1] instanceof Date) {
+                return {
+                  x: {
+                    type: 'timeseries',
+                    tick: {
+                      fit: true
+                    }
+                  }
+                };
+              }
 
+            })()
+          });
+        },
+        update: function(){
+          var unpacked = _unpack(this.data.table);
+          this._chart.load({
+            columns: unpacked
+          });
+        },
+        remove: function(){
+          handleRemoval.call(this);
+        }
+      });
+
+    });
 
     // Register library + types
     // -------------------------------
 
-    Keen.Visualization.register('c3', {
-      'linechart'   : LineChart
-    });
+    Keen.Visualization.register('c3', charts);
 
   })(Keen);
