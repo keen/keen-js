7dd3b46be31dde15718421c1c67b9c96b1c8bea9
Eric Anderson <eric@ericanderson.ca>
2014-08-26 08:46:29 -0700

    Removing the triggering of remove for now.
    Can't get it working in the cleanviz branch and don't see where it's used.

--- src/visualize.js
+++ src/visualize.js
@@ -64,9 +64,6 @@
   };
 
   function _build_visual(selector, config){
-    if (this.visual) {
-      this.visual.trigger("remove");
-    }
     this.visual = new Keen.Visualization(this, selector, config);
   }
 
