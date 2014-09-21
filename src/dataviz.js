edd82c2f6b75bfdea043ffe64f8fabdaadbd32a5
Dustin Larimer <dustinlarimer@gmail.com>
2014-09-21 15:31:31 -0700

    Move Keen.Spinner.defaults into keen adapter Clean out view._artifacts object on destroy

--- src/dataviz.js
+++ src/dataviz.js
@@ -13,26 +13,6 @@ _extend(Keen.utils, {
 // Set flag for script loading
 Keen.loaded = false;
 
-Keen.Spinner.defaults = {
-  lines: 10,                    // The number of lines to draw
-  length: 8,                    // The length of each line
-  width: 3,                     // The line thickness
-  radius: 10,                   // The radius of the inner circle
-  corners: 1,                   // Corner roundness (0..1)
-  rotate: 0,                    // The rotation offset
-  direction: 1,                 // 1: clockwise, -1: counterclockwise
-  color: '#4d4d4d',             // #rgb or #rrggbb or array of colors
-  speed: 1.67,                  // Rounds per second
-  trail: 60,                    // Afterglow percentage
-  shadow: false,                // Whether to render a shadow
-  hwaccel: false,               // Whether to use hardware acceleration
-  className: 'keen-spinner',    // The CSS class to assign to the spinner
-  zIndex: 2e9,                  // The z-index (defaults to 2000000000)
-  top: '50%',                   // Top position relative to parent
-  left: '50%'                   // Left position relative to parent
-};
-
-
 // ------------------------------
 // Dataviz constructor
 // ------------------------------
@@ -310,6 +290,7 @@ Keen.Dataviz.prototype.destroy = function(){
   this.view._prepared = false;
   this.view._initialized = false;
   this.view._rendered = false;
+  this.view._artifacts = {};
   return this;
 };
 
--- src/plugins/keen-widgets.js
+++ src/plugins/keen-widgets.js
@@ -9,6 +9,26 @@
     var Keen = lib || {},
         Metric, Error, Spinner;
 
+    Keen.Spinner.defaults = {
+      lines: 10,                    // The number of lines to draw
+      length: 8,                    // The length of each line
+      width: 3,                     // The line thickness
+      radius: 10,                   // The radius of the inner circle
+      corners: 1,                   // Corner roundness (0..1)
+      rotate: 0,                    // The rotation offset
+      direction: 1,                 // 1: clockwise, -1: counterclockwise
+      color: '#4d4d4d',             // #rgb or #rrggbb or array of colors
+      speed: 1.67,                  // Rounds per second
+      trail: 60,                    // Afterglow percentage
+      shadow: false,                // Whether to render a shadow
+      hwaccel: false,               // Whether to use hardware acceleration
+      className: 'keen-spinner',    // The CSS class to assign to the spinner
+      zIndex: 2e9,                  // The z-index (defaults to 2000000000)
+      top: '50%',                   // Top position relative to parent
+      left: '50%'                   // Left position relative to parent
+    };
+
+
     var dataTypes = {
       'singular': ['metric']
     };
