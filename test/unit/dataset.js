5fed45c574b3c895267e32d65acef0277f6d5f1b
Dustin Larimer <dustinlarimer@gmail.com>
2014-09-21 15:42:57 -0700

    Add basic tests for Keen.Dataset

--- test/unit/dataset.js
+++ test/unit/dataset.js
@@ -1,5 +1,26 @@
 describe("Keen.Dataset", function(){
-  it("should pass", function(){
-    expect(0).to.eql(0);
+  beforeEach(function(){
+    this.ds = new Keen.Dataset({ result: 23456 }, {
+      collection: "",
+      select: true
+    });
+  });
+  afterEach(function(){
+    this.ds = null;
+  });
+  describe("constructor", function(){
+    it("should return a new Keen.Dataset instance", function(){
+      expect(this.ds).to.be.an.instanceof(Keen.Dataset);
+    });
+    it("should have a schema hash with supplied properties", function(){
+      expect(this.ds.schema).to.deep.equal({ collection: "", select: true });
+    });
+    it("should have a table property with correct values", function(){
+      expect(this.ds.table).to.be.an("array");
+      expect(this.ds.table[0][0]).to.be.a("string")
+        .and.to.eql("result");
+      expect(this.ds.table[1][0]).to.be.a("number")
+        .and.to.eql(23456);
+    });
   });
 });
