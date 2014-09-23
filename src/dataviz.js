746bc5f567fa7d2aa4508d8a43adc757852839a5
Dustin Larimer <dustinlarimer@gmail.com>
2014-09-23 13:38:24 -0700

    Edit dataset.schema and re-parse on dataviz.orderBy call

--- src/dataviz.js
+++ src/dataviz.js
@@ -123,8 +123,31 @@ Keen.Dataviz.prototype.parseRequest = function(req){
 Keen.Dataviz.prototype.orderBy = function(str){
   if (!arguments.length) return this.view.attributes.orderBy;
   this.view.attributes.orderBy = (str ? String(str) : Keen.Dataviz.defaults.orderBy);
+  _runOrderBy.call(this);
   return this;
 };
+
+function _runOrderBy(){
+  var self = this,
+      root = this.dataset.meta.schema || this.dataset.meta.unpack,
+      newOrder = this.orderBy().split(".").join(Keen.Dataset.defaults.delimeter);
+  // Replace in schema and re-run dataset.parse()
+  each(root, function(def, i){
+    // update 'select' configs
+    if (i === "select" && def instanceof Array) {
+      each(def, function(c, j){
+        if (c.path.indexOf("timeframe -> ") > -1) {
+          self.dataset.meta.schema[i][j].path = newOrder;
+        }
+      });
+    }
+    // update 'unpack' configs
+    else if (i === "unpack" && typeof def === "object") {
+      self.dataset.meta.schema[i]['index'].path = newOrder;
+    }
+  });
+  this.dataset.parse();
+}
 
 Keen.Dataviz.prototype.sortGroups = function(str){
   if (!arguments.length) return this.view.attributes.sortGroups;
