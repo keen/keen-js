b060f9ffa1400f7345a47f9a2bc25ed691faae78
Dustin Larimer <dustinlarimer@gmail.com>
2014-09-25 01:07:24 -0700

    New color palette to increase contrast of variants

--- src/dataviz/index.js
+++ src/dataviz/index.js
@@ -71,12 +71,9 @@ _extend(Keen.Dataviz, {
   defaults: {
     colors: [
     /* teal      red        yellow     purple     orange     mint       blue       green      lavender */
-    // norm
       "#00bbde", "#fe6672", "#eeb058", "#8a8ad6", "#ff855c", "#00cfbb", "#5a9eed", "#73d483", "#c879bb",
-    // dark
-      "#00a1bf", "#df545f", "#d29847", "#7373bc", "#e0704b", "#00b2a1", "#4988d0", "5ebb6d", "#b163a4",
-    // light
-      "#24c9e8", "#ff7e88", "#f4bd70", "#9a9adf", "#ff9876", "#24dcca", "71aef3", "#86de95", "#d68ac9"
+      "#0099b6", "#d74d58", "#cb9141", "#6b6bb6", "#d86945", "#00aa99", "#4281c9", "#57b566", "#ac5c9e",
+      "#27cceb", "#ff818b", "#f6bf71", "#9b9be1", "#ff9b79", "#26dfcd", "#73aff4", "#87e096", "#d88bcb"
     ],
     indexBy: "timeframe.start"
   },
