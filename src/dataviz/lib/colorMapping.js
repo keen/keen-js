d1019f58a179d61eef9cae2b4cebf47c1092866c
Dustin Larimer <dustinlarimer@gmail.com>
2014-09-29 11:28:17 -0600

    Update colorMapping to avoid inserting colors for subsequent calls

--- src/dataviz/lib/colorMapping.js
+++ src/dataviz/lib/colorMapping.js
@@ -14,22 +14,23 @@ function _runColorMapping(){
       schema = this.dataset.schema,
       data = this.dataset.output(),
       colorSet = this.colors(),
-      colorMap = this.colorMapping();
+      colorMap = this.colorMapping(),
+      dt = this.dataType() || "";
 
   if (colorMap) {
-    // Map to selected index
-    if (schema.select && data[0].length == 2) {
-      _each(data, function(row, i){
-        if (i > 0 && colorMap[String(row[0])]) {
-          colorSet.splice(i-1, 0, colorMap[row[0]]);
+
+    if (dt.indexOf("chronological") > -1 || (schema.unpack && data[0].length > 2)) {
+      _each(data[0], function(cell, i){
+        if (i > 0 && colorMap[String(cell)] && colorSet[i-1] !== colorMap[String(cell)]) {
+          colorSet.splice(i-1, 0, colorMap[String(cell)]);
         }
       });
     }
-    // Map to unpacked labels
-    if (schema.unpack) {
-      _each(data[0], function(cell, i){
-        if (i > 0 && colorMap[String(cell)]) {
-          colorSet.splice(i-1, 0, colorMap[cell]);
+    //else if (schema.select && data[0].length === 2) {
+    else {
+      _each(data, function(row, i){
+        if (i > 0 && colorMap[String(row[0])] && colorSet[i-1] !== colorMap[row[0]]) {
+          colorSet.splice(i-1, 0, colorMap[row[0]]);
         }
       });
     }
