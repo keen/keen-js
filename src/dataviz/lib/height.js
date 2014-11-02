b6e3aba27b4e7d456b8d15a0e03cd20e67fa86e0
Dustin Larimer <dustinlarimer@gmail.com>
2014-11-02 12:11:15 -0800

    Replace uglify with yui-compressor grunt task

--- src/dataviz/lib/height.js
+++ src/dataviz/lib/height.js
@@ -1,5 +1,5 @@
-Keen.Dataviz.prototype.height = function(int){
-  if (!arguments.length) return this.view.attributes.height;
-  this.view.attributes['height'] = (!isNaN(parseInt(int)) ? parseInt(int) : null);
+Keen.Dataviz.prototype.height = function(num){
+  if (!arguments.length) return this.view.attributes['height'];
+  this.view.attributes['height'] = (!isNaN(parseInt(num)) ? parseInt(num) : null);
   return this;
 };
