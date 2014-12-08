ccea66b90bf721ac0f77215ef0ce033ccf9f9d19
Dustin Larimer <dustinlarimer@gmail.com>
2014-12-08 03:16:42 -0800

    Fix self-reference bug, add GET/POST structure for browser requests

--- src/core/utils/requestHandler.js
+++ src/core/utils/requestHandler.js
@@ -8,37 +8,61 @@ var Keen = require("../index"),
     sendBeacon = require("./sendBeacon"),
     getUrlMaxLength = require("./getUrlMaxLength");
 
+var methods = {
+  "GET"    : get,  // xhr, jsonp, or beacon
+  "POST"   : post, // xhr
+  "PUT"    : post  // xhr
+  // "DELETE" : del
+};
+
 module.exports = function(url, data, api_key, success, error, async){
+  var method = ( this.config.requestType === "xhr" && getXHR() ) ? "POST" : "GET";
+  methods[method].apply(this, arguments);
+  return;
+}
+
+function get(url, data, api_key, success, error){
   var reqType = this.config.requestType,
       queryString = "",
       successCallback = success,
       errorCallback = error,
-      isAsync = async || true,
       stringMax;
 
   success = error = null;
 
   if ( (reqType === "jsonp" || reqType === "beacon") || !getXHR() ) {
+
     queryString += "?api_key="  + encodeURIComponent( this.writeKey() );
     queryString += "&data="     + encodeURIComponent( base64.encode( JSON2.stringify(data) ) );
     queryString += "&modified=" + encodeURIComponent( new Date().getTime() );
+
     if ( String(url + queryString).length < getUrlMaxLength() ) {
       if (reqType === "jsonp") {
-        sendJsonp(url + queryString, null, successCallback, errorCallback);
+        sendJsonp.call(this, url + queryString, null, successCallback, errorCallback);
       }
       else {
-        sendBeacon(url + queryString, null, successCallback, errorCallback);
+        sendBeacon.call(this, url + queryString, null, successCallback, errorCallback);
       }
       return;
     }
+
   }
   if ( getXHR() ) {
-    sendXhr("POST", url, {
-        "Authorization": this.writeKey(),
-        "Content-Type": "application/json"
-      }, data, successCallback, errorCallback, async);
+    post.apply(this, arguments);
   }
   else {
     this.trigger("error", "Request not sent: URL length exceeds current browser limit, and XHR (POST) is not supported.");
   }
+}
+
+function post(url, data, api_key, success, error, async){
+  var successCallback = success,
+      errorCallback = error,
+      isAsync = async || true;
+
+  success = error = null;
+  sendXhr.call(this, "POST", url, {
+      "Authorization": api_key,
+      "Content-Type": "application/json"
+    }, data, successCallback, errorCallback, isAsync);
 }
--- src/core/utils/sendXhr.js
+++ src/core/utils/sendXhr.js
@@ -3,7 +3,8 @@ var each = require("./each"),
     JSON2 = require("JSON2");
 
 function sendXhr(method, url, headers, body, success, error, async){
-  var isAsync = async || true,
+  var self = this,
+      isAsync = async || true,
       successCallback = success,
       errorCallback = error,
       xhr = getXhr(),
@@ -12,7 +13,7 @@ function sendXhr(method, url, headers, body, success, error, async){
   success = error = null;
 
   if (!xhr) {
-    this.trigger("error", "XHR requests are not supported");
+    self.trigger("error", "XHR requests are not supported");
     return;
   }
 
@@ -23,7 +24,7 @@ function sendXhr(method, url, headers, body, success, error, async){
         try {
           response = JSON2.parse(xhr.responseText);
         } catch (e) {
-          this.trigger("error", "Could not parse HTTP response: " + xhr.responseText);
+          self.trigger("error", "Could not parse HTTP response: " + xhr.responseText);
           if (errorCallback) {
             errorCallback(xhr, e);
             successCallback = errorCallback = null;
@@ -34,17 +35,18 @@ function sendXhr(method, url, headers, body, success, error, async){
           successCallback = errorCallback = null;
         }
       } else {
-        this.trigger("error", "HTTP request failed.");
+        self.trigger("error", "HTTP request failed.");
         try {
           response = JSON2.parse(xhr.responseText);
         }
         catch (e) {
-          response = null;
+          response = void 0;
           if (errorCallback) {
             errorCallback(xhr, e);
             successCallback = errorCallback = null;
           }
         }
+
         if (errorCallback && response) {
           errorCallback(xhr, response);
           successCallback = errorCallback = null;
