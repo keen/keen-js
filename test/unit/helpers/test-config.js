7bfc68fdcade51aa8ecbd5628566ecb89749adfb
Dustin Larimer <dustinlarimer@gmail.com>
2015-03-12 15:38:36 -0600

    Drop HTTPS for HTTP so sauce IE9 tests can run properly

--- test/unit/helpers/test-config.js
+++ test/unit/helpers/test-config.js
@@ -3,7 +3,7 @@ module.exports = {
   writeKey   : "554a723d023da6cb24e51c56a9a54555e9dcf8403d4b71ffa37e9112295622e78a10eed43a13c83b14ce171b0f1317bb09aa8df43d50f73b77709ab431af611ea47ed65f4d74c0ea5f2bde8407322ab70559afef294673ee6c224308b1744c9e069508799edefc51264b3f75a1ba9e26",
   readKey    : "f979d53e026bdbf1ba16f01ce168bc7bcf9d5308ba672fb14e1834793a9e705eefa04793f0f87fb76b1d49a6cc2747b96a8abae0e4569d70314099b3f7790f55e98c9ac482e3883aab86a4bb577c295dbbca4867e95e2e4b15038fac5d80957ded3e868e4e6e319d3aa9275abc22b16e",
   masterKey  : "FC135394DD08E3976870B7E7E83BDCD8",
-  protocol   : "https",
+  protocol   : "http",
   host       : "mocha.keen.io/9999/3.0",
   collection : "mocha",
   properties: {
