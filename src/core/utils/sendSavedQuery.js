bce4f3d2626d1634b410ecafdeda482912218267
Joanne Cheng <joanne@keen.io>
2015-11-20 13:20:11 -0500

    Allow user to use read or master key for saved query results

--- src/core/utils/sendSavedQuery.js
+++ src/core/utils/sendSavedQuery.js
@@ -2,10 +2,18 @@ var request = require('superagent');
 var responseHandler = require('../helpers/superagent-handle-response');
 
 module.exports = function(path, params, callback){
+  var key;
+  if (this.client.readKey()) {
+    key = this.client.readKey();
+  }
+  else if (this.client.masterKey()) {
+    key = this.client.masterKey();
+  }
+
   request
     .get(this.client.url(path))
     .set('Content-Type', 'application/json')
-    .set('Authorization', this.client.readKey())
+    .set('Authorization', key)
     .timeout(this.timeout())
     .send()
     .end(function(err, res) {
