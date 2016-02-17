1bde56d9bf402ec745e98b56c738503da3bf86c4
dustinlarimer <dustinlarimer@gmail.com>
2016-02-17 10:48:25 -0800

    Provide failing test for new master key length

--- test/unit/modules/server/scoped-keys-spec.js
+++ test/unit/modules/server/scoped-keys-spec.js
@@ -5,47 +5,66 @@ var Keen = require("../../../../src/server"),
 
 describe("Scoped Keys", function(){
 
-  beforeEach(function() {
-    this.client = new Keen({
-      projectId  : keenHelper.projectId,
-      masterKey  : keenHelper.masterKey
-    });
-  });
-
-  it("should encrypt a scoped key with given options", function() {
-
-    var options = {
+  beforeEach(function(){
+    this.keyOptions = {
       "allowed_operations": ["read"],
-      "filters": [ {
-        "property_name": "purchase.amount",
-        "operator": "eq",
-        "property_value": 56
-      }, {
-        "property_name": "purchase.name",
-        "operator": "ne",
-        "property_value": "Barbie"
-      }]
+      "filters": [
+        {
+          "property_name": "purchase.amount",
+          "operator": "eq",
+          "property_value": 56
+        },
+        {
+          "property_name": "purchase.name",
+          "operator": "ne",
+          "property_value": "Barbie"
+        }
+      ]
     };
-    // Encrypt a scoped key..
-    var scopedKey = Keen.utils.encryptScopedKey(this.client.masterKey(), options);
-    // ..then decrypt it
-    var decryptedOptions = Keen.utils.decryptScopedKey(this.client.masterKey(), scopedKey);
-    expect(decryptedOptions).to.deep.equal(options);
   });
 
-  it("should decrypt a scoped key and return the correct options", function() {
+  describe("New key validation", function(){
 
-    var apiKey = "f5d7c745ba4f437a82db02ca8b416556";
-    var scopedKey = "7b8f357fa55e35efb2f7fa51a03ec2835c5537e57457c5a7c1c40c454fc00d5addef7ed911303fc2fa9648d3ae13e638192b86e90cd88657c9dc5cf03990cbf6eb2a7994513d34789bd25447f3dccaf5a3de3b9cacf6c11ded581e0506fca147ea32c13169787bbf8b4d3b8f2952bc0bea1beae3cfbbeaa1f421be2eac4cc223";
-    var options = {
-      "filters": [{
-        "property_name": "account_id",
-        "operator": "eq",
-        "property_value": "4d9a4c421d011c553e000001"
-      }]
-    };
-    var decryptedOptions = Keen.utils.decryptScopedKey(apiKey, scopedKey);
-    expect(decryptedOptions).to.deep.equal(options);
+    beforeEach(function(){
+      this.apiKey = '78AFA8B8FF164C2429590C48F74C26165D37C8B95B5DFC71E498A32C57C9C38E';
+    });
+
+    it("should encrypt an old scoped key with given options", function() {
+      // Encrypt a scoped key..
+      var scopedKey = Keen.utils.encryptScopedKey(this.apiKey, this.keyOptions);
+      // ..then decrypt it
+      var decryptedOptions = Keen.utils.decryptScopedKey(this.apiKey, scopedKey);
+      expect(decryptedOptions).to.deep.equal(this.keyOptions);
+    });
+
+    it("should decrypt an old scoped key and return the correct options", function() {
+      var scopedKey = Keen.utils.encryptScopedKey(this.apiKey, this.keyOptions);
+      var decryptedOptions = Keen.utils.decryptScopedKey(this.apiKey, scopedKey);
+      expect(decryptedOptions).to.deep.equal(this.keyOptions);
+    });
+
+  });
+
+  describe("Legacy key validation", function(){
+
+    beforeEach(function(){
+      this.apiKey = 'FC135394DD08E3976870B7E7E83BDCD8';
+    });
+
+    it("should encrypt an old scoped key with given options", function() {
+      // Encrypt a scoped key..
+      var scopedKey = Keen.utils.encryptScopedKey(this.apiKey, this.keyOptions);
+      // ..then decrypt it
+      var decryptedOptions = Keen.utils.decryptScopedKey(this.apiKey, scopedKey);
+      expect(decryptedOptions).to.deep.equal(this.keyOptions);
+    });
+
+    it("should decrypt an old scoped key and return the correct options", function() {
+      var scopedKey = Keen.utils.encryptScopedKey(this.apiKey, this.keyOptions);
+      var decryptedOptions = Keen.utils.decryptScopedKey(this.apiKey, scopedKey);
+      expect(decryptedOptions).to.deep.equal(this.keyOptions);
+    });
+
   });
 
 });
